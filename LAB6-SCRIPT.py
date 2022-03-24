893e900496377f24d1e39d88e0c9c9385cf002ff
Claus2001 <claus.delaguardia@flemingcollege.ca>
2022-03-24 19:27:09 -0400

    Update LAB6-SCRIPT.py

--- LAB6-SCRIPT.py
+++ LAB6-SCRIPT.py
@@ -35,7 +35,7 @@ def get_pastebin_string(user_dict):
     #paste.
     tittle = user_dict['species']['name'] + "'s Abilities"
     body_text = "Name: " +  user_dict['species']['name'] + "\n"
-    body_text += "Abilities:" + user_dict['abilities']['0']['ability']['name']
+    body_text += "Abilities:" + user_dict['abilities'][0]['ability']['name']
     return (tittle, body_text)
 
 def get_user_info(user_num):
