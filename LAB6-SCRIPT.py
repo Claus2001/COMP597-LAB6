b93d8230706fce32d5ee981c7b3af80bf1045b7b
Claus2001 <claus.delaguardia@flemingcollege.ca>
2022-03-24 17:31:18 -0400

    Update LAB6-SCRIPT.py

--- LAB6-SCRIPT.py
+++ LAB6-SCRIPT.py
@@ -33,7 +33,7 @@ def post_to_pastebin(tittle, body_text):
 def get_pastebin_string(user_dict):
     #2) Create a function that builds the strings to be used for the title and body text of a PasteBin 
     #paste.
-    tittle = user_dict['name'] + "'s Abilities"
+    tittle = user_dict['forms']['name'], + "'s Abilities"
     body_text = "Name: " +  user_dict['forms']['name'], "\n"
     body_text += "Abilities:" + user_dict['abilities']['ability']['name']
     return (tittle, body_text)
